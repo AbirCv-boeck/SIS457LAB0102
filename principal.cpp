31b15f63fdc2a51dfd1628e90b67439a38bbe158
AbirCv-boeck <abircvonboeck@gmail.com>
2022-09-28 01:07:59 -0400

    Lab finish

---
+++ principal.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int  main(){
+    cout << "Mi nombre es Abir Zaid Contreras Von boeck,\ntengo 22 años y soy de la ciudad de trinidad-beni,\nactualmente vivo en la ciudad de sucre-chuquisaca, estudio ing. de sistemas en la USFX,\nMi CU: 35-4707";
+	return 0;
+}
